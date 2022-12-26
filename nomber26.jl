ec6d390217ead94be16300f2c405684bbb7a5ef2
Lukachev <119494026+Lukachev@users.noreply.github.com>
2022-12-26 09:05:12 +0300

    Add files via upload

---
+++ nomber26.jl
@@ -0,0 +1,24 @@
+function get_fibbonachi(n::Int)::Int
+    if n == 1 || n == 2
+        return 1
+    end
+
+    a = 1
+    b = 1
+    for i in 3:n
+        tmp = a + b
+        a, b = b, tmp
+    end
+
+    return b
+end
+
+#б
+
+function get_fibbonachi_rec(n::Int)::Int
+    if n == 1 || n == 2
+        return 1
+    end
+
+    return get_fibbonachi_rec(n-1) + get_fibbonachi_rec(n - 2)
+end
