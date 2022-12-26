95c065c445eca54b3db3c121e155bbee70c13548
Lukachev <119494026+Lukachev@users.noreply.github.com>
2022-12-26 05:50:36 +0300

    Add files via upload

---
+++ nomber18.jl
@@ -0,0 +1,9 @@
+function move_recursive!(r::Robot, side::HorizonSide)
+    if !isborder(r, side)
+        move!(r, side)
+        move_recursive!(r, side)
+    end
+end
+
+r = Robot("pole.sit", animate=true)
+move_recursive!(r, Nord)
